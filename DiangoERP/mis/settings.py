51ea2f36afd85e87bf408e245364349c6c5316a2
Robin <379111601@qq.com>
2018-03-28 14:53:13 +0300

    add database address and port

--- DiangoERP/mis/settings.py
+++ DiangoERP/mis/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'mis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
+        'HOST': '127.0.0.1:3306',
         'NAME': 'mis',
         'USER': 'root',
         'PASSWORD': 'robin',
